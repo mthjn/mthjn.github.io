5e731b1fda73cf5bf7dae2cf6f6ac4265928b2b2
mthjn <jana.mathauserova@ubk.cz>
2015-07-21 15:22:57 +0200

    Add ripple

---
+++ ripple/js/index.js
@@ -0,0 +1,29 @@
+;(function($, window, document, undefined) {
+
+  'use strict';
+
+  var $ripple = $('.js-ripple');
+
+  $ripple.on('click.ui.ripple', function(e) {
+
+    var $this = $(this);
+    var $offset = $this.parent().offset();
+    var $circle = $this.find('.c-ripple__circle');
+
+    var x = e.pageX - $offset.left;
+    var y = e.pageY - $offset.top;
+
+    $circle.css({
+      top: y + 'px',
+      left: x + 'px'
+    });
+
+    $this.addClass('is-active');
+
+  });
+
+  $ripple.on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(e) {
+  	$(this).removeClass('is-active');
+  });
+
+})(jQuery, window, document);
